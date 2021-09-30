16b02d212771011d6e2a1456a848f8deac486702
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-09-30 20:25:18 +0600

    react search project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import SingleMealDetails from './components/SingleMealDetails/SingleMealDetails';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
       <Router>
         <Switch>
           <Route path="/home"><Home /></Route>
+          <Route exact path="/"><Home /></Route>
           <Route path="/homes/:mealId"><SingleMealDetails /></Route>
+          <Route path="*"><PageNotFound /></Route>
 
         </Switch>
       </Router>
